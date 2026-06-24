7c220a5eb78af9f544fe933ff5dc36d03450ce47
sri <sriswethamani1998@gmail.com>
2026-06-24 22:11:56 +0530

    DEBUG - TRUE

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3c8ks0k8rw(4ab-@n0dwwbqkis#ucto-jki#mp&32y_gx+0sxh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pmc-dashboard-mxvm.onrender.com']
 
